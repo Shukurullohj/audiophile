5e49284c3714ce8f7a659db9f6fdf024ca12b839
shukurullohj <shukurulloh.jamolov@mail.ru>
2022-06-28 12:22:05 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,183 @@
+<template>
+    <div>
+        <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
+        <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
+        <a href="" class="button--shop">
+          <span class="button--shop__text">SHOP</span>
+          <svg width="7" height="12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="m1.322 1 5 5-5 5" stroke="#D87D4A" stroke-width="2"/></svg>
+        </a>
+        <app-footer v-bind:title="title"></app-footer>
+    </div>
+</template>
+
+<script>
+// Imports
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Ninjas from './components/Ninjas.vue';
+
+export default {
+    components: {
+        'app-header': Header,
+        'app-footer': Footer,
+        'app-ninjas': Ninjas
+    },
+    data () {
+        return {
+          ninjas: [
+              {name: 'Ryu', speciality: 'Vue Components', show: false},
+              {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
+              {name: 'Hitoshi', speciality: 'Click Events', show: false},
+              {name: 'Tango', speciality: 'Conditionals', show: false},
+              {name: 'Kami', speciality: 'Webpack', show: false},
+              {name: 'Yoshi', speciality: 'Data Diggin', show: false}
+          ],
+          title: 'Vue Wizards'
+        }
+    },
+    methods: {
+      updateTitle: function(updatedTitle){
+        this.title = updatedTitle;
+      }
+    }
+}
+</script>
+
+<style>
+html {
+  box-sizing: border-box;
+  height: 100%;
+  scroll-behavior: smooth;
+}
+:root {
+  --color-orange: #d87d4a;
+  --color-black: #101010;
+  --color-light: #f1f1f1;
+  --color-white-light: #fafafa;
+  --color-orange-light: #fbaf85;
+  --color-white: #ffffff;
+  --color-dark: #000000;
+}
+
+@font-face {
+    font-family: 'Manrope';
+    src: local("Manrope"), url('/fonts/Manrope-Bold.woff2') format('woff2');
+    font-weight: bold;
+    font-style: normal;
+    font-display: swap;
+}
+
+@font-face {
+    font-family: 'Manrope';
+    src: local("Manrope"), url('/fonts/Manrope-Regular.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+}
+
+@font-face {
+    font-family: 'Manrope';
+    src: local("Manrope"), url('/fonts/Manrope-Medium.woff2') format('woff2');
+    font-weight: 500;
+    font-style: normal;
+    font-display: swap;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+
+body {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+  font-weight: 500;
+  line-height: 25px;
+  font-size: 15px;
+  font-family: 'Manrope', "Arial", sans-serif;
+  background-color: var(--color-light);
+  color: var(--color-dark);
+}
+
+a {
+  margin: 0;
+  text-decoration: none;
+}
+p {
+  margin: 0;
+}
+h1, h2, h3, h4, h5, h6 {
+  margin: 0;
+}
+.container {
+  max-width: 1200px;
+  padding-left: 24px;
+  padding-right: 24px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.button {
+  display: inline-block;
+  border: none;
+  background-color: var(--color-orange);
+  color: var(--color-white);
+  padding: 15px 31px;
+}
+
+.button:hover {
+  background-color: var(--color-orange-light);
+}
+
+.button--white {
+  background-color: var(--color-white);
+  color: var(--color-black);
+  border: 1px var(--color-black) solid;
+}
+
+.button--white {
+  background-color: var(--color-dark);
+  color: var(--color-white);
+}
+
+.button-shop {
+  display: inline-flex;
+  justify-content: center;
+  text-align: center;
+  border: none;
+  background-color: transparent;
+  text-transform: uppercase;
+}
+
+.button-shop__text {
+  margin-right: 10px;
+  color: var(--color-dark);
+  opacity: .5;
+}
+
+.button-shop__text:hover {
+  color: var(--color-orange);
+  opacity: 1;
+}
+@media screen and (min-width:768px) {
+    .container {
+    padding-left: 40px;
+    padding-right: 40px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+
+@media screen and (min-width:1100px) {
+    .container {
+    padding-left: 165px;
+    padding-right: 165px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,208 @@
+<template>
+    <header>
+        <div class="container">
+          <div class="nav">
+            <span @click="showMenu = !showMenu" class="button-sidebar">
+              <svg class="button-sidebar__icon" width="16" height="15" fill="none" xmlns="http://www.w3.org/2000/svg"><g fill="#fff"><path d="M0 0h16v3H0zM0 6h16v3H0zM0 12h16v3H0z"/></g></svg>
+              <ul class="nav__list">
+                <li class="nav__item">
+                  <a href="#" class="nav__link">Home</a>
+                </li>
+                <li class="nav__item">
+                  <a href="" class="nav__link">headphones</a>
+                </li>
+                <li class="nav__item">
+                  <a href="" class="nav__link">speakers</a>
+                </li>
+                <li class="nav__item">
+                  <a href="" class="nav__link">earphones</a>
+                </li>
+              </ul>
+            </span>
+            <img class="sitelogo" src="../img/sitelogo.svg" alt="">
+            <button class="button-cart">
+              <svg width="23" height="20" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M7.411 13.195a.667.667 0 0 0-.673.66c0 .363.303.659.674.659h12.219c.372 0 .674.295.674.66 0 .364-.302.66-.674.66H7.412c-1.114 0-2.021-.889-2.021-1.98 0-.812.502-1.511 1.218-1.816L4.176 1.32H.674A.667.667 0 0 1 0 .66C0 .296.302 0 .674 0h4.043c.316 0 .59.215.658.517l.481 2.122h16.47c.212 0 .41.097.538.262.127.166.168.38.11.579l-2.695 9.236a.672.672 0 0 1-.648.478H7.41Zm1.214 2.638c1.132 0 2.054.935 2.054 2.084 0 1.148-.922 2.083-2.054 2.083-1.132 0-2.054-.935-2.054-2.083 0-1.15.922-2.084 2.054-2.084Zm9.857 0c1.133 0 2.054.935 2.054 2.084 0 1.148-.922 2.083-2.054 2.083-1.132 0-2.053-.935-2.053-2.083 0-1.15.92-2.084 2.053-2.084Zm.64-3.958 2.31-7.917H6.157l1.797 7.917h11.17ZM9.31 17.917a.69.69 0 0 0-.685-.695.69.69 0 0 0-.685.695.69.69 0 0 0 .685.694.69.69 0 0 0 .685-.694Zm9.172-.695a.69.69 0 0 1 .685.695.69.69 0 0 1-.685.694.69.69 0 0 1-.684-.694.69.69 0 0 1 .684-.695Z" fill="#fff"/></svg>
+            </button>
+          </div>
+          <div v-if="showMenu" class="menu">
+            <div class="menu__list">
+               <div class="menu__item">
+                <img class="menu__img" width="80" height="104" src="../img/headphone-icon.svg" alt="">
+                <p class="menu__text">headphones</p>
+                <button class="button-shop">
+                  <p class="button-shop__text">shop</p>
+                  <svg width="7" height="12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="m1.322 1 5 5-5 5" stroke="#D87D4A" stroke-width="2"/></svg>
+                </button>
+              </div>
+              <div class="menu__item">
+                <img class="menu__img" width="80" height="104" src="../img/speaker-icon.svg" alt="">
+                <p class="menu__text">speakers</p>
+                <button class="button-shop">
+                  <p class="button-shop__text">shop</p>
+                  <svg width="7" height="12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="m1.322 1 5 5-5 5" stroke="#D87D4A" stroke-width="2"/></svg>
+                </button>
+              </div>
+              <div class="menu__item">
+                <img class="menu__img" width="80" height="104" src="../img/earphone-icon.svg" alt="">
+                <p class="menu__text">earphones</p>
+                <button class="button-shop">
+                  <p class="button-shop__text">shop</p>
+                  <svg width="7" height="12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="m1.322 1 5 5-5 5" stroke="#D87D4A" stroke-width="2"/></svg>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+    </header>
+</template>
+<script>
+export default {
+    props: {
+      
+    },
+    data(){
+        return{
+          showMenu : false,
+        }
+    },
+
+}
+</script>
+<style scoped>
+  header {
+    background-color: var(--color-black);
+  }
+
+  .nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 30px 0;
+  }
+
+  .nav__list {
+    display: none;
+  }
+
+  .button-cart {
+    border: none;
+    flex: none;
+    background-color: transparent;
+  }
+
+  .menu {
+    position: fixed;
+    z-index: 9998;
+    top: 85px;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+
+  .menu__list {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding-top: 85px;
+    padding-bottom: 35px;
+    background-color: var(--color-white);
+  }
+
+  .menu__item {
+    position: relative;
+    width: 327px;
+    height: 165px;
+    background-color: var(--color-light);
+    text-align: center;
+    text-transform: uppercase;
+    font-weight: 700;
+    border-radius: 10px;
+  }
+
+  .menu__item:not(:last-child) {
+    margin-bottom: 68px;
+  }
+
+  .menu__img {
+    position: relative;
+    top: -30%;
+    filter: drop-shadow(1px 18px 28px var(--color-black));
+  }
+
+  .menu__text {
+    margin-top: -30px;
+    margin-bottom: 15px;
+  }
+
+  .button-shop {
+    padding: 0;
+    margin: 0;
+    margin-bottom: 20px;
+  }
+  @media only screen and (min-width:768px) {
+
+    .menu__list {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      padding-bottom: 68px;
+      border-radius: 0 0 10px 10px;
+    }
+    .menu__item {
+      width: 223px;
+    }
+    .menu__item:not(:last-child) {
+      margin-bottom: 0;
+      margin-right: 10px;
+    }
+    .nav {
+      justify-content: start;
+    }
+
+    .button-sidebar {
+      margin-right: 40px;
+    }
+    .button-cart {
+      flex: 1;
+      text-align: right;
+    }
+  }
+
+  @media only screen and (min-width:1100px) {
+    .nav__list {
+      display: flex;
+      list-style: none;
+    }
+
+    .nav__item {
+      margin-right: 34px;
+    }
+
+    .nav__link {
+      font-size: 13px;
+      color: var(--color-white);
+      text-transform: uppercase;
+      letter-spacing: 2px;
+      font-style: normal;
+      font-weight: 700;
+    }
+
+    .nav__link:hover {
+      color: var(--color-orange);
+    }
+
+    .button-sidebar__icon {
+      display: none;
+    }
+    .sitelogo {
+      order: -1;
+      margin-right: 200px;
+    }
+    .menu {
+      display: none;
+    }
+  }
+</style>
